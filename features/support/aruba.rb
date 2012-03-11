18bb2071f9dd30a7a0075b0362298d5be72a8439
Cezary Baginski <cezary.baginski@gmail.com>
2012-03-11 17:52:21 +0100

    Replace PATH in tests

--- features/support/aruba.rb
+++ features/support/aruba.rb
@@ -5,8 +5,12 @@ Before do
 
   @old_home = ENV['HOME']
   ENV['HOME'] = File.expand_path(dirs.first)
+
+  @old_path = ENV['PATH']
+  ENV['PATH'] = "#{File.expand_path(dirs.first)}:#{ENV['PATH']}"
 end
 
 After do
+  ENV['PATH'] = @old_path
   ENV['HOME'] = @old_home
 end
